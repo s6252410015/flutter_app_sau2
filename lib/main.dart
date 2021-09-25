300aac689b2d2f8425ae00779fe3ebaea451eef9
s6252410015 <ss6252410015@sau.ac.th>
2021-09-25 19:55:14 +0700

    Code V.1: Do 9/25

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_sau2/views/login_ui.dart';
+
+main(){
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home : LoginUI() , //หน้าจอแรก
+    ),
+  );
+}
